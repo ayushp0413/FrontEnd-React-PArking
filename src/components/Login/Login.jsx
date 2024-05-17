40d8bb4e08a7ee1d9433d6d12f0ad5a2cc87bdbf
Anirudh joshi <yashjoshi5521@gmail.com>
2024-05-18 03:44:20 +0530

    Authentication

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,32 +1,66 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react'
-import "./Login.css"
-import { Link } from 'react-router-dom'
-
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import './Login.css';
+import { Link, useNavigate } from 'react-router-dom';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import axios from 'axios';
 
 const Login = () => {
+  const navigate = useNavigate();
+
+  const handleSubmit = async (values, { setSubmitting, setErrors }) => {
+    try {
+      const response = await axios.post('http://localhost:5000/api/auth/login', values);
+      console.log(response.data);
+      // Redirect to home page upon successful login
+      navigate('/home');
+    } catch (err) {
+      if (err.response && err.response.status === 401) {
+        setErrors({ email: 'The provided email or password is incorrect or not found in the database.' });
+      } else {
+        console.error(err.response.data);
+      }
+    }
+    setSubmitting(false);
+  };
+
   return (
-    <>
     <div className="login-details-container">
-          <img src = "src\components\First\background.jpg"/>
-          <div className="details-container">
-            {/* <h3>Login Details Page</h3> */}
-            <form className="details-form">
+      <img src="src/components/First/background.jpg" alt="background" />
+      <div className="details-container">
+        <Formik
+          initialValues={{ email: '', password: '' }}
+          validate={values => {
+            const errors = {};
+            if (!values.email) {
+              errors.email = 'Required';
+            }
+            if (!values.password) {
+              errors.password = 'Required';
+            }
+            return errors;
+          }}
+          onSubmit={handleSubmit}
+        >
+          {({ isSubmitting }) => (
+            <Form className="details-form">
               <div className="form-group">
                 <label htmlFor="email">Email / Username:</label>
-                <input type="text" id="email" name="email" placeholder="Enter your email or username" />
+                <Field type="text" id="email" name="email" placeholder="Enter your email or username" />
+                <ErrorMessage name="email" component="div" className="error-message" />
               </div>
               <div className="form-group">
                 <label htmlFor="password">Password:</label>
-                <input type="password" id="password" name="password" placeholder="Enter your password" />
+                <Field type="password" id="password" name="password" placeholder="Enter your password" />
+                <ErrorMessage name="password" component="div" className="error-message" />
               </div>
-                <Link to={'/home'}><button type="submit">Login</button></Link>
-            </form>
-          </div>
-        
+              <button type="submit" disabled={isSubmitting}>Login</button>
+            </Form>
+          )}
+        </Formik>
+      </div>
     </div>
-    </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,39 +1,172 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react'
-import './Signup.css'
-import { Link } from 'react-router-dom'
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
+import './Signup.css';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const Signup = () => {
-  return (
-    <>
-    <div className="login-details-container">
-          <img src = "src\components\First\background.jpg"/>
-          <div className="details-container">
-            {/* <h3>Login Details Page</h3> */}
-            <form className="details-form">
-              <div className="form-group">
-                <label htmlFor="email">Username:</label>
-                <input type="text" id="email" name="email" placeholder="Enter your username" />
-              </div>
-              <div className="form-group">
-                <label htmlFor="email">Email:</label>
-                <input type="text" id="email" name="email" placeholder="Enter your email" />
-              </div>
-              <div className="form-group">
-                <label htmlFor="email">Password:</label>
-                <input type="text" id="email" name="email" placeholder="Enter your password" />
-              </div>
-              <div className="form-group">
-                <label htmlFor="password">Confirm Password:</label>
-                <input type="password" id="password" name="password" placeholder="confirm your password" />
-              </div>
-                <Link to={'/home'}><button type="submit">SignUp</button></Link>
-            </form>
-          </div>
-        
-    </div>
-    </>
-  )
-}
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+    contactNumber: '',
+    vehicleNumber: '',
+    address: '',
+    age: ''
+  });
 
-export default Signup
+  const [showPassword, setShowPassword] = useState(false);
+
+  const navigate = useNavigate();
+
+  const { 
+    username, 
+    email, 
+    password, 
+    confirmPassword, 
+    contactNumber, 
+    vehicleNumber, 
+    address, 
+    age 
+  } = formData;
+
+  const onChange = e => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      console.error('Passwords do not match');
+      return;
+    }
+
+    try {
+      const response = await axios.post('http://localhost:5000/api/auth/register', {
+        name: username,
+        email,
+        password,
+        contactNumber,
+        vehicleNumber,
+        address,
+        age
+      });
+      console.log(response.data);
+      navigate('/login');
+    } catch (err) {
+      console.error(err.response.data);
+    }
+  };
+
+  return (
+    <div className="Signup-details-container">
+      <img src='src\components\First\background.jpg' alt="background"></img>
+      <div className="details-container">
+        <form className="details-form" onSubmit={onSubmit}>
+          <div className="form-group">
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              placeholder="Enter your username"
+              value={username}
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Enter your email"
+              value={email}
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="contactNumber">Contact Number:</label>
+            <input
+              type="text"
+              id="contactNumber"
+              name="contactNumber"
+              placeholder="Enter your contact number"
+              value={contactNumber}
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="vehicleNumber">Vehicle Number:</label>
+            <input
+              type="text"
+              id="vehicleNumber"
+              name="vehicleNumber"
+              placeholder="Enter your vehicle number"
+              value={vehicleNumber}
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="address">Address:</label>
+            <input
+              type="text"
+              id="address"
+              name="address"
+              placeholder="Enter your address"
+              value={address}
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="age">Age:</label>
+            <input
+              type="text"
+              id="age"
+              name="age"
+              placeholder="Enter your age"
+              value={age}
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password:</label>
+            <div style={{ position: 'relative' }}>
+              <input
+                type={showPassword ? 'text' : 'password'}
+                id="password"
+                name="password"
+                placeholder="Enter your password"
+                value={password}
+                onChange={onChange}
+              />
+              <button
+                className='unique-button'
+                type="button"
+                onClick={() => setShowPassword(!showPassword)}
+                style={{ position: 'absolute', right: '5px', top: '40%', transform: 'translateY(-50%)' }}
+              >
+                {showPassword ? 'Hide' : 'Show'}
+              </button>
+            </div>
+          </div>
+          <div className="form-group">
+            <label htmlFor="confirmPassword">Confirm Password:</label>
+            <input
+              type="password"
+              id="confirmPassword"
+              name="confirmPassword"
+              placeholder="Confirm your password"
+              value={confirmPassword}
+              onChange={onChange}
+            />
+          </div>
+          <button type="submit">Sign Up</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
